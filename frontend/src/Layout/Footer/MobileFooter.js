c442ad4918506ef89492faf87bf1f86c22dd5c2b
hms-hostmm <eainthmoo@host.com.mm>
2024-02-29 17:57:30 +0630

    update moblie footer

--- frontend/src/Layout/Footer/MobileFooter.js
+++ frontend/src/Layout/Footer/MobileFooter.js
@@ -1,17 +1,28 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { BsCollectionPlay } from "react-icons/bs";
-import { FiHeart, FiUserCheck } from "react-icons/fi";
+import { FiHeart, FiUserCheck, FiLogOut } from "react-icons/fi";
 import { CgMenuBoxed } from "react-icons/cg";
 import MenuDrawer from "../../Components/Drawer/MenuDrawer";
 import { SidebarContext } from "../../Context/SiderbarContextProvider";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../Actions/userActions";
 function MobileFooter() {
+  const dispatch = useDispatch();
   const { mobileDrawer, toggleDrawer } = useContext(SidebarContext);
   const active = "bg-white text-main";
   const inActive =
     "transitions text-2xl flex-colo hover:text-main text-white rounded-md px-4 py-3";
   const Hover = ({ isActive }) =>
     isActive ? `${active} ${inActive}` : inActive;
+
+  const { favouriteCount } = useSelector((state) => state.movies);
+  const { loading, token, isAuthenticated } = useSelector(
+    (state) => state.user
+  );
+  const logoutHandler = () => {
+    dispatch(logout(token));
+  };
   return (
     <>
       <div className="flex-btn h-full bg-white rounded cursor-pointer overflow-y-scroll flex-grow w-full">
@@ -25,14 +36,21 @@ function MobileFooter() {
               <div className="relative">
                 <FiHeart />
                 <div className="absolute -top-5 -right-1 bg-subMain text-white text-xs rounded-full w-5 h-5 flex-colo">
-                  3
+                  {favouriteCount}
                 </div>
               </div>
             </NavLink>
-            <NavLink to="/login" className={Hover}>
-              <FiUserCheck />
-            </NavLink>
-            <button onClick={toggleDrawer} className={inActive}>
+            {!isAuthenticated && (
+              <NavLink to="/login" className={Hover}>
+                <FiUserCheck />
+              </NavLink>
+            )}
+            {isAuthenticated && (
+              <button onClick={logoutHandler} className={inActive}>
+                <FiLogOut />
+              </button>
+            )}
+            <button className={inActive}>
               <CgMenuBoxed />
             </button>
           </div>
